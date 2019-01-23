5f2f809f59b51b95a3cd70a6914375c76dc835a7
Kingaa6 <Kingaa6@gmail.com>
2019-01-23 12:49:05 -0800

    Server setup and data pulled into project

--- friends/src/App.jsx
+++ friends/src/App.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => console.log(err));
   }
 
   render() {
+    console.log(this.state.friends);
     return (
       <div className="App">
         <h1>working</h1>
